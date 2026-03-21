e87e226f66431fa877bba9e65f6e1b37127dabaa
GritandGo <codemain0123@gmail.com>
2026-03-21 10:48:56 -0500

    feat: implement continuous polling and state persistence in worker

--- worker.py
+++ worker.py
@@ -1,42 +1,51 @@
+import time
 from urllib.request import Request, urlopen
 from urllib.error import URLError, HTTPError
 import json
 import urllib.request
 
-#read
-with open("tasks.json", "r") as file:
-    tasks = json.load(file) #tasks is a list containing dictionaries
+was_updated = False
+while True:
+    was_updated = False
+    with open("tasks.json", "r") as file:
+        tasks = json.load(file) #tasks is a list containing dictionaries
 
-#completing the work
+    #completing the work
 
-for task in tasks:
-    if task["status"] == "pending":
-        try:
-            req = urllib.request.Request(
-                task["url"],
-                headers={"User-Agent": "Mozilla/5.0"}
-            )
+    for task in tasks:
+        if task["status"] == "pending":
+            try:
+                req = urllib.request.Request(
+                    task["url"],
+                    headers={"User-Agent": "Mozilla/5.0"}
+                )
 
-            with urllib.request.urlopen(req, timeout=5) as response:
-                server_type = response.headers.get("Server", "Unknown")
-                task["result"] = f"Server type: {server_type}"
+                with urllib.request.urlopen(req, timeout=5) as response:
+                    server_type = response.headers.get("Server", "Unknown")
+                    task["result"] = f"Server type: {server_type}"
 
-            task["status"] = "complete"
-
-        except HTTPError as e:
-            task["status"] = "failed"
-            task["result"] = f"HTTP Error: {e.code}"
-
-        except URLError as e:
-            task["status"] = "failed"
-            task["result"] = f"Url Error: {e.reason}"
-
-        except Exception as e:
-            task["status"] = "failed"
-            task["result"] = f"Unexpected Error: {e}"           
-            
-
-with open("tasks.json", "w") as f:
-    json.dump(tasks, f, indent=4)
+                task["status"] = "complete"
+                was_updated = True #update to True if a task was completed
 
 
+            except HTTPError as e:
+                task["status"] = "failed"
+                task["result"] = f"HTTP Error: {e.code}"
+                was_updated = True
+
+            except URLError as e:
+                task["status"] = "failed"
+                task["result"] = f"Url Error: {e.reason}"
+                was_updated = True
+
+            except Exception as e:
+                task["status"] = "failed"
+                task["result"] = f"Unexpected Error: {e}" 
+                was_updated = True          
+
+    if was_updated == True:                
+        with open("tasks.json", "w") as f:
+            json.dump(tasks, f, indent=4)
+
+
+    time.sleep(5)
